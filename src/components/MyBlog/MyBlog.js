4ca3f496c7668d69be824be5ea006134cf85bba2
NguyenDat251 <nguyenquocdat2511998@gmail.com>
2023-06-16 00:27:12 +0700

    feat: add first blog

--- src/components/MyBlog/MyBlog.js
+++ src/components/MyBlog/MyBlog.js
@@ -1,12 +1,20 @@
+import blog1Bg from "assets/blog1_bg.webp";
+
 export default function MyBlog() {
   return (
     <div className="h-screen flex flex-col justify-start items-start pt-24">
       <h2 className="titleSection">Pieces I've written</h2>
-      <div>
+      <div className="w-full bg-[--light-navy] p-6 rounded-md">
+        <div
+          className="bg-cover h-[300px] mb-4"
+          style={{
+            backgroundImage: `url(${blog1Bg})`,
+          }}
+        />
         <h3 className="text-[--green]">
           Build a color picker Chrome extension in 10 minutes
         </h3>
-        <p className="p-6 bg-[--light-navy]">
+        <p className="">
           A simple tutorial on how to create a color picker Chrome's extension
         </p>
         <span className="text-xs">
