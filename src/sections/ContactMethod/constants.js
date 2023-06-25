ea2816cda31125efbfd7c9aa638425b45e761ada
NguyenDat251 <nguyenquocdat2511998@gmail.com>
2023-06-26 00:36:04 +0700

    fix: footer

--- src/sections/ContactMethod/constants.js
+++ src/sections/ContactMethod/constants.js
@@ -1,7 +1,6 @@
-export const contactNames = ["Github", "LinkedIn", "Gmail", "Instagram"];
+export const contactNames = ["Github", "LinkedIn", "Gmail"];
 export const contactLink = [
   "https://github.com/NguyenDat251",
   "https://www.linkedin.com/in/%C4%91%E1%BA%A1t-nguy%E1%BB%85n-a77ba518a/",
   "nguyenquocdat2511998@gmail.com ",
-  "https://www.instagram.com/no_quo_do/",
 ];
